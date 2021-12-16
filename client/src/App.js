7241a361d89f43cd117cb80e5823f6bd64fbff0f
MusaabAbbas <44504478+MusaabAbbas@users.noreply.github.com>
2021-12-16 10:43:18 -0500

    RollingJack App

---
+++ client/src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Rolling Jack Wheels Store</h1>
+        <img src="https://discounttire.scene7.com/is/image/discounttire/Hero_Products_4x100_Rims_Wheels_001?$ContentHero$&wid=960&dpr=off" alt="Arrow-gloss-black.jpg" width="1000" height="414"></img>
+        <h2>Luxury Wheels, and Racing Wheels</h2>
+        <h3>Wheels that range from 16in to 21in with offsets of -40 ~ +40</h3>
+        <h4>Aluminum/Steel wheels with colors options like; Black, White, Chrome/Silver, and Gold/Bronze.</h4>
+      </header>
+
+      <h5>Luxury Wheels</h5>
+      <h6>ARROW Gloss Black with Brushed Face</h6>
+      <img src="https://ic.carid.com/niche/items/niche-arrow-gloss-black-brushed-face_1.jpg" alt="Arrow-gloss-black.jpg" width="200" height="200"></img>
+      <h7>Choose Wheel Size</h7>
+      <select name="Sizes" id="cars">
+        <option value="size">Sizes</option>
+        <option value="9.5x19">9.5 x 19 in</option>
+        <option value="10x19">10 x 19 in</option>
+        <option value="10x20">10 x 20 in</option>
+        <option value="10x21">10 x 21 in</option>
+      </select>
+
+      <h6>REDBOURNE - ALSTON Gloss White</h6>
+      <img src="https://ic.carid.com/redbourne/items/redbourne-alston-5-lug-gloss-white_1.jpg" alt="Gloss-White.jpg" width="200" height="200"></img>
+      <h7>Choose Wheel Size</h7>
+      <select name="Sizes" id="cars">
+        <option value="size">Sizes</option>
+        <option value="9.5x19">9.5 x 19 in</option>
+        <option value="10x19">10 x 19 in</option>
+        <option value="10x20">10 x 20 in</option>
+        <option value="10x21">10 x 21 in</option>
+      </select>
+
+      <h6>NICHE - SUV VICE Chrome</h6>
+      <img src="https://ic.carid.com/niche/items/niche-m234-suv-chrome_1.jpg" alt="SUV-VICE-Chrome.jpg" width="200" height="200"></img>
+      <h7>Choose Wheel Size</h7>
+      <select name="Sizes" id="cars">
+        <option value="size">Sizes</option>
+        <option value="9.5x19">9.5 x 19 in</option>
+        <option value="10x19">10 x 19 in</option>
+        <option value="10x20">10 x 20 in</option>
+        <option value="10x21">10 x 21 in</option>
+      </select>
+
+      <h6>RENNEN - FT12 Bronze Tint</h6>
+      <img src="https://ic.carid.com/rennen-wheels/items/rennen-flowtech-ft-12-bronze-tint_1.jpg" alt="FT12-Bronze-Tint.jpg" width="200" height="200"></img>
+      <h7>Choose Wheel Size</h7>
+      <select name="Sizes" id="cars">
+        <option value="size">Sizes</option>
+        <option value="9.5x19">9.5 x 19 in</option>
+        <option value="10x19">10 x 19 in</option>
+        <option value="10x20">10 x 20 in</option>
+        <option value="10x21">10 x 21 in</option>
+      </select>
+
+
+      <h5>Racing Wheels</h5>
+      <h6>RTX - AMAZE II Polished</h6>
+      <img src="https://ic.carid.com/rtx-wheels/items/rtx-amaze-ii-polished_1.jpg" alt="RTX-AMAZE-II-Polished.jpg" width="200" height="200"></img>
+      <h7>Choose Wheel Size</h7>
+      .last {
+      <select name="Sizes" id="last">
+        <option value="size">Sizes</option>
+        <option value="9.5x19">9.5 x 19 in</option>
+        <option value="10x19">10 x 19 in</option>
+        <option value="10x20">10 x 20 in</option>
+        <option value="10x21">10 x 21 in</option>
+      </select>
+      }
+
+    </div>
+  );
+}
+
+export default App;
